cadacce239506b297613b1a9e9745143de6ae61f
Dmitry_71 <dgorodilov@mail.ru>
2024-07-18 10:58:53 +0300

    initial commit

---
+++ src/main/java/ru/netology/springboot_2_hw/SpringBoot2HwApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.springboot_2_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2HwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/springboot_2_hw/SpringBoot2HwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.springboot_2_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2HwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
